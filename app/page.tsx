669c71bda47473f1898948687a4c659607c09e1d
EgorTarasov <61450170+EgorTarasov@users.noreply.github.com>
2025-02-28 09:04:21 +0300

    analytics

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,16 @@ import { SessionProvider } from "next-auth/react"
 
 export default function Home() {
   return (
+    <>
+    <script defer data-domain="inno.larek.tech" src="https://plausible-iwgkwss80g8s0k40www0ok0g.larek.tech/js/script.file-downloads.hash.outbound-links.js"></script>
+    <script>window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }</script>
+
       <main className="min-h-screen bg-background">
         <Suspense fallback={<Loading />}>
           <Dashboard />
         </Suspense>
       </main>
+    </>
   )
 }
 
